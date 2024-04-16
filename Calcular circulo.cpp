1b7e24ad45560cc5e363c5faecaf522ba5c97f1f
MateusMunhoz <145906004+MateusMunhoz@users.noreply.github.com>
2024-04-16 14:41:26 -0300

    Update Calcular circulo.cpp
    Mudança na logica usando if e else

--- Calcular circulo.cpp
+++ Calcular circulo.cpp
@@ -9,8 +9,12 @@ int main(){
     printf("Insira o raio do circulo");
     scanf("%f", &R);
 
-    if(R < 0){}
-    
-    float A = Pi * (R * R);
-    printf("A area do circulo e igual a: %f", A);
+    if(R < 0)
+    {
+		printf("%f eh menor do que 0, resultado errado",R);
+	}
+	else{
+		float A = Pi * (R * R);
+    	printf("A area do circulo e igual a: %f", A);
+	}
 }
