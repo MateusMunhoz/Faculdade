629fe3e3eaac1644608f89c34bccc2be46d56dbb
MateusMunhoz <145906004+MateusMunhoz@users.noreply.github.com>
2024-04-18 16:37:00 -0300

    Update Calcular circulo.cpp
    ESTRUTURA FOR ADCIONADA

--- Calcular circulo.cpp
+++ Calcular circulo.cpp
@@ -6,17 +6,21 @@ const float Pi = 3.14;
 
 int main(){
     float R;
-    printf("Insira o raio do circulo");
-    scanf("%f", &R);
+	int i;
+    
 
-    if(R > 0)
-    {
-	    float A = Pi * (R * R);
-	    printf("A area do circulo e igual a: %f", A);
+
+	for(i = 0; i<3; i++){    //Sempre colocar i = 0 pq se n vai usar um valor aleatório da memória
 		
-	}else{
-	    printf("%f eh menor do que 0, resultado errado",R);
-		
+		printf("Insira o raio do circulo \n ");
+    	scanf("%f", &R);
     	
+		if(R > 0){
+			float A = Pi * (R * R);
+			printf("A area do circulo e igual a: %f \n", A);
+		}
+		else{
+			printf("%f eh menor do que 0, resultado invalido \n");
+		}
 	}
 }
